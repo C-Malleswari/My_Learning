15098e0b8bcfac9993fe1a399e38230c0bc6b6a0
MALLESWARI C <malleswariraji23@gmail.com>
2025-08-07 15:42:37 +0530

    Create Repeated_word.c

---
+++ Repeated_word.c
@@ -0,0 +1,47 @@
+/*
+ 1.write a c program to find the most frequent occur letter in a word
+input : viven
+output: v->2 times
+*/
+
+#include <stdio.h>
+#include <string.h>
+
+void most_frequent_letter(char *str)
+{
+    int freq[256] = {0};
+    int max = 0;
+    char result;
+    int i;
+    for ( i = 0; str[i] != '\0'; i++)
+    {
+        freq[(unsigned char)str[i]]++; // Count frequency of each character
+
+        if (freq[(unsigned char)str[i]] > max)
+        {
+            max = freq[(unsigned char)str[i]];
+            result = str[i];
+        }
+    }
+    //printf("Most frequent character: '%c' -> %d times\n", result, max);
+    if(max == 1)
+    {
+            printf("none\n");
+    }
+    else
+    {
+            printf("Most frequent character: '%c' -> %d times\n", result, max);
+    }
+
+}
+
+int main()
+{
+    char word[100];
+    printf("Enter a word: ");
+    fgets(word,sizeof(word),stdin);
+
+    most_frequent_letter(word);
+
+    return 0;
+}
